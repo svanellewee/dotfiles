e57f57de88b7e99741ce6c972366c52cf0f22304
svanellewee <stephan.van.ellewee@gmail.com>
2018-06-08 08:00:01 +0200

    upgrade

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+NVIM_TARGET=$HOME/.config/nvim
+[[ ! -L "${NVIM_TARGET}" ]] && ln -s $HOME/dotfiles/nvim/ ${NVIM_TARGET}
 
-ln -s $HOME/dotfiles/nvim/ $HOME/.config/nvim
-echo "~/dotfiles/bash/bashrc" >> ~/.bashrc
+
+SOURCE_BASHRC="source ~/dotfiles/bash/bashrc" 
+
+[[ ! -f ~/.bashrc ]] && touch ~/.bashrc
+
+grep -q "${SOURCE_BASHRC}" ~/.bashrc
+if [[ "$?" == "0" ]]; then
+	echo "dotfiles bashrc already installed"
+else
+	echo "Upgrayedd-ing ;-)"
+	echo ${SOURCE_BASHRC} >> ~/.bashrc
+fi
