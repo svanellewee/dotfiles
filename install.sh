2a06d4095294e602136d252dcac60be98de3c473
Stephan van Ellewee <stephan.van.ellewee@gmail.com>
2020-03-02 08:22:49 +0200

    colours!

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@ NVIM_TARGET=$HOME/.config/nvim
 [[ ! -L "${NVIM_TARGET}" ]] && ln -s $HOME/dotfiles/nvim/ ${NVIM_TARGET}
 [[ ! -L "${NVIM_TARGET}" ]] && ln -s $HOME/dotfiles/i3/ $HOME/.config/i3/
 
+# install applications
+if [[ ! -d $HOME/applications ]]
+then
+	mkdir -p $HOME/applications/
+	python3 -m venv $HOME/applications/venv3
+	$HOME/applications/venv3/bin/pip install -U pip
+	$HOME/applications/venv3/bin/pip install -U datasette
+fi
+
 ln -s $HOME/dotfiles/tmux/dot.tmux.conf $HOME/.tmux.conf
 SOURCE_BASHRC="source ~/dotfiles/bash/bashrc" 
 
